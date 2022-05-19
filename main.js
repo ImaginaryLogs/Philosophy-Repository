bf69094bc2b673c2d1d574dd5ff216bec10019cc
ImaginaryLogs <96267180+ImaginaryLogs@users.noreply.github.com>
2022-05-20 01:07:50 +0800

    guesser

--- main.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('h1')
-document.addEventListener('DOMContentLoaded', () => {
-    
-})
-
--- topic_math/root-algorithm.js
+++ topic_math/root-algorithm.js
@@ -1,5 +1,18 @@
+var display_num = 0;
 
-function randomize(){
-    var display_num = Math.pow(Math.floor(Math.random() * 66) + 32, 2);
-    document.getElementById("num").innerHTML = display_num;
+function randomize() {
+    display_num = randomIntBetween(32, 99)
+    document.getElementById("num").innerHTML = Math.pow(display_num, 2) + " " + display_num;
+}
+
+function randomIntBetween(first, last) {
+    return Math.floor(Math.random() * (Math.abs(last - first + 1))) + first;
+}
+
+function num_evaluate() {
+    if (display_num == 0) { return alert("Press the random square button.") }
+    var guess_num = parseInt(document.getElementById("num_guess").value);
+    if (!(guess_num === parseInt(guess_num, 10))) { return alert("Valid input required"); }
+    if (!(guess_num === display_num)) { return alert(guess_num + " is wrong. Try again."); }
+    return alert("True")
 }
